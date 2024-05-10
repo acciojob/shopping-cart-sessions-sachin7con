5b6c523b62276c0c5bd00444970e331201bb9dc3
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-10 15:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function addToCart(productId) {
 	if(product){
 		cart.push(product);
 		renderCart();
-		sessionStorage.setItem('product', JSON.stringify(cart));
+		sessionStorage.setItem('cart', JSON.stringify(cart));
 	}
 }
 
